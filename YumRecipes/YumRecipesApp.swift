1970c3952f1b007a9a84fbda3a08b452a27bbedf
NatCanCode <nathalie.piat1@gmail.com>
2022-10-28 15:08:30 +0200

    Initial Commit

---
+++ YumRecipes/YumRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumRecipesApp.swift
+//  YumRecipes
+//
+//  Created by N N on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct YumRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
